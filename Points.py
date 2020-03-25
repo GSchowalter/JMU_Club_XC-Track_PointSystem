f8ecaa19bfd9319d789c490436ea1da7be1138c5
Grant Schowalter <gschowalter@Grants-MBP.fios-router.home>
2020-03-25 16:36:44 -0400

    Forgot the class heading. Its there now

--- Points.py
+++ Points.py
@@ -9,15 +9,15 @@ This module is part of the JMU_Club_XC-Track_PointSystem package.
 This module holds the class that is the value for the RosterPoints dictionary entries.
 
 """
+class Points:
+    def __init__(self, cs_points = 0, fr_points = 0):
+        self.cs_points = cs_points
+        self.fr_points = fr_points
 
-def __init__(self, cs_points = 0, fr_points = 0):
-    self.cs_points = cs_points
-    self.fr_points = fr_points
+    def add_fr(self, points):
+        self.fr_points += points
 
-def add_fr(self, points):
-    self.fr_points += points
-
-def add_cs(self, points):
-    self.cs_points += points
+    def add_cs(self, points):
+        self.cs_points += points
 
 
