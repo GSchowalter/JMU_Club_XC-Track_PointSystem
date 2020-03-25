6fc2fc3928f2a00fe8220418a7fe646579383a4d
Grant Schowalter <gschowalter@Grants-MBP.fios-router.home>
2020-03-25 16:35:53 -0400

    Changed f to fr. Implemented the add event methods

--- Points.py
+++ Points.py
@@ -1,5 +1,6 @@
 """
 Author: Grant Schowalter
+
 Date Started: 3/21/2020
 Version: 0.0.1
 
@@ -9,12 +10,12 @@ This module holds the class that is the value for the RosterPoints dictionary en
 
 """
 
-def __init__(self, cs_points = 0, f_points = 0):
+def __init__(self, cs_points = 0, fr_points = 0):
     self.cs_points = cs_points
-    self.f_points = f_points
+    self.fr_points = fr_points
 
-def add_f(self, points):
-    self.f_points += points
+def add_fr(self, points):
+    self.fr_points += points
 
 def add_cs(self, points):
     self.cs_points += points
--- RosterPoints.py
+++ RosterPoints.py
@@ -1,3 +1,4 @@
+import Points
 """
 Author: Grant Schowalter
 Date Started: 3/21/2020
@@ -10,3 +11,25 @@ The each member of the roster is entered as a string into a dict called roster.
 Points object is assigned as the value for each of these enteries (See Points). 
 
 """
+
+def __init__(self, cs_requirement = 3, fr_requirement = 3, roster = []):
+    self.cs_requirement = cs_requirement
+    self.fr_requirement = fr_requirement
+    self.roster = roster
+    self.pointsOf = dict(roster, Points())
+
+# add a specified number of points to the participant's cs total
+def add_cs_event(self, participants, points=1):
+    for participant in participants:
+        if self.pointsOf[participant] == None:
+            self.pointsOf[participant] = Points(1)
+        else:
+            self.pointsOf[participant].add_cs(points)
+
+# add a specified number of points to the participant's fr total
+def add_fr_event(self, participants, points=1):
+    for participant in participants:
+        if self.pointsOf[participant] == None:
+            self.pointsOf[participant] = Points(fr_points = 1)
+        else:
+            self.pointsOf[participant].add_cs(points)
